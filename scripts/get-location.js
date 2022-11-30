9ec203388735a9acb4cd10a3c8a96acff9698622
Andrey Calderon <eltercercafe@gmail.com>
2022-11-30 15:44:22 -0500

    url que dirije al json con los datos del clima y agregue una section donde mostrar la informacion

--- scripts/get-location.js
+++ scripts/get-location.js
@@ -5,26 +5,39 @@ const accessLocation = document.getElementsByClassName('location__button')[0]
 
 // sacado de internet ⬇⬇⬇⬇: because: la API del clima requiere la unicación.
 
-const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-};
+// const options = {
+//     enableHighAccuracy: true,
+//     timeout: 5000,
+//     maximumAge: 0
+// };
 
-function success(pos) {
-    const crd = pos.coords;
-    console.log('Tu ubicación actual es:');
-    console.log(`Latitud : ${crd.latitude}`);
-    console.log(`Longitud: ${crd.longitude}`);
-    console.log(`Más o menos ${crd.accuracy} metros.`);
-    console.log( getWeather(crd.latitude, crd.longitude))
-}
+// function success(pos) {
+//     const crd = pos.coords;
+//     console.log('Tu ubicación actual es:');
+//     console.log(`Latitud : ${crd.latitude}`);
+//     console.log(`Longitud: ${crd.longitude}`);
+//     console.log(`Más o menos ${crd.accuracy} metros.`);
+//     console.log( getWeather(crd.latitude, crd.longitude))
+// }
 
-function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-}
+// function error(err) {
+//     console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
 
+// accessLocation.addEventListener('click',()=>{
+//     navigator.geolocation.getCurrentPosition(success, error, options);
+// })
 accessLocation.addEventListener('click',()=>{
-    navigator.geolocation.getCurrentPosition(success, error, options);
+    let longitude
+    let latitude
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( position => {
+            latitude = position.coords.latitude
+            longitude = position.coords.longitude
+
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=es&units=metric`
+
+            console.log(url)
+        })
+    }
 })
-  
