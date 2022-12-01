d7442c1ad9cafe15f11091ea75f5c439f26b1c37
cristianbyte <charlsmaritz@gmail.com>
2022-12-01 00:46:09 -0500

    reorganizacion de ideas, assets corregidos. Preguntar por el trablero del clima.

--- scripts/api-data.js
+++ scripts/api-data.js
@@ -1,5 +1,10 @@
-async function getWeather(latitude, longitude){
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=es&units=metric`)
+async function getWeather(latitude, longitude, forecast=false){
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/${forecast?'forecast':'weather'}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=es&units=metric`)
     const data = response.json()
     return data
+}
+
+function srcImageMon(id){
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
 }
--- scripts/get-location.js
+++ scripts/get-location.js
@@ -3,41 +3,13 @@ const API_KEY = '03a17b7c9ecf18f15cac5b10e1f35e74'
 
 const accessLocation = document.getElementsByClassName('location__button')[0]
 
-// sacado de internet ⬇⬇⬇⬇: because: la API del clima requiere la unicación.
-
-// const options = {
-//     enableHighAccuracy: true,
-//     timeout: 5000,
-//     maximumAge: 0
-// };
-
-// function success(pos) {
-//     const crd = pos.coords;
-//     console.log('Tu ubicación actual es:');
-//     console.log(`Latitud : ${crd.latitude}`);
-//     console.log(`Longitud: ${crd.longitude}`);
-//     console.log(`Más o menos ${crd.accuracy} metros.`);
-//     console.log( getWeather(crd.latitude, crd.longitude))
-// }
-
-// function error(err) {
-//     console.warn(`ERROR(${err.code}): ${err.message}`);
-// }
-
-// accessLocation.addEventListener('click',()=>{
-//     navigator.geolocation.getCurrentPosition(success, error, options);
-// })
 accessLocation.addEventListener('click',()=>{
-    let longitude
-    let latitude
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition( position => {
-            latitude = position.coords.latitude
-            longitude = position.coords.longitude
-
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=es&units=metric`
-
-            console.log(url)
+            let latitude = position.coords.latitude
+            let longitude = position.coords.longitude
+            const data = getWeather(latitude, longitude)
+            console.log(data);
         })
     }
 })
